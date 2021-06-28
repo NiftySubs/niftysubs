b28995108a81d19f243de820bc2c916c33346521
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2021-06-28 23:09:19 +0530

    SuperChat Contract Added

---
+++ migrations/2_super_app.js
@@ -0,0 +1,5 @@
+const SuperApp = artifacts.require("SuperApp");
+
+module.exports = function (deployer) {
+  deployer.deploy(SuperApp, "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6", "0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A", "0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90", 3858024691358);
+};
