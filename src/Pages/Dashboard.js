d934129082f3136c44336c97b9ebd77dec826c8f
lopeselio <lopeselio@gmail.com>
2021-07-04 01:48:21 +0530

    added component

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -21,7 +21,9 @@ import { BrowserRouter as Router, Link, Route, Switch, useRouteMatch } from "rea
 import { create } from "ipfs-http-client";
 import Streams from  "../Components/Streams";
 import { FaHandHoldingHeart } from "react-icons/fa";
-
+import Flow from "../Components/Flow";
+import SuperChatGraph from "../Components/SuperChatGraph";
+import FundGraph from "../Components/FundGraph";
 
 function Dashboard() {
 
@@ -30,7 +32,7 @@ function Dashboard() {
 
 
     return (
-        <HStack py="50px" px="100px">
+        <HStack py="50px" px="100px" spacing={40}>
             <Router>
 
                 <VStack alignItems="flex-start">
@@ -45,140 +47,13 @@ function Dashboard() {
                             <Streams />
                         </Route>
                         <Route exact  path={`${path}/flows`}>
-                            {
-                                <Heading>Flows</Heading>
-                            }
-                            {   
-                           <Table variant="striped" width="100vh" height="50vh" size="lg">
-                           <Thead>
-                             <Tr>
-                               <Th isNumeric>Stream Flow ID</Th>
-                               <Th isNumeric>Flow Sum</Th>
-                               <Th isNumeric>Flow Rate</Th>
-                               <Th isNumeric>Flow lastUpdate</Th>
-                               <Th isNumeric>Flow Token</Th>
-                               <Th isNumeric>Flow Owner</Th>
-                               <Th isNumeric>Flow Receipient</Th>
-                             </Tr>
-                           </Thead>
-                           <Tbody>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumberic><b>ETH</b> 0.2 </Td>
-                               <Td isNumeric>3</Td>
-                               <Td isNumeric>01-01-2021</Td>
-                               <Td isNumeric>ETH</Td>
-                               <Td isNumeric>0x234</Td>
-                               <Td isNumeric>0x123</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumberic><b>ETH</b> 0.2 </Td>
-                               <Td isNumeric>3</Td>
-                               <Td isNumeric>01-01-2021</Td>
-                               <Td isNumeric>ETH</Td>
-                               <Td isNumeric>0x234</Td>
-                               <Td isNumeric>0x123</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumberic><b>ETH</b> 0.2 </Td>
-                               <Td isNumeric>3</Td>
-                               <Td isNumeric>01-01-2021</Td>
-                               <Td isNumeric>ETH</Td>
-                               <Td isNumeric>0x234</Td>
-                               <Td isNumeric>0x123</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumberic><b>ETH</b> 0.2 </Td>
-                               <Td isNumeric>3</Td>
-                               <Td isNumeric>01-01-2021</Td>
-                               <Td isNumeric>ETH</Td>
-                               <Td isNumeric>0x234</Td>
-                               <Td isNumeric>0x123</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumberic><b>ETH</b> 0.2 </Td>
-                               <Td isNumeric>3</Td>
-                               <Td isNumeric>01-01-2021</Td>
-                               <Td isNumeric>ETH</Td>
-                               <Td isNumeric>0x234</Td>
-                               <Td isNumeric>0x123</Td>
-                             </Tr>
-                           </Tbody>
-                            <TableCaption>Stream Flow Analytics</TableCaption>
-                         </Table> 
-                        }
+                            <Flow />
                         </Route>
                         <Route exact  path={`${path}/superchats`}>
-                            {
-                                <Heading>SuperChats</Heading>
-                            }
-                            {   
-                           <Table variant="striped" width="100vh" height="50vh" size="lg">
-                           <Thead>
-                             <Tr>
-                               <Th isNumeric>Super Chat ID</Th>
-                               <Th isNumeric>Transaction</Th>
-                               <Th isNumeric>Address</Th>
-                               <Th isNumeric>Underlying Address</Th>
-                               <Th isNumeric>SuperToken Name</Th>
-                               <Th isNumeric>Symbol</Th>
-                             </Tr>
-                           </Thead>
-                           <Tbody>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumeric>0X123</Td>
-                               <Td isNumeric>0X234</Td>
-                               <Td isNumeric>0X567</Td>
-                               <Td isNumeric>Hola</Td>
-                               <Td isNumeric>ETH</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumeric>0X123</Td>
-                               <Td isNumeric>0X234</Td>
-                               <Td isNumeric>0X567</Td>
-                               <Td isNumeric>Hola</Td>
-                               <Td isNumeric>ETH</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumeric>0X123</Td>
-                               <Td isNumeric>0X234</Td>
-                               <Td isNumeric>0X567</Td>
-                               <Td isNumeric>Hola</Td>
-                               <Td isNumeric>ETH</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumeric>0X123</Td>
-                               <Td isNumeric>0X234</Td>
-                               <Td isNumeric>0X567</Td>
-                               <Td isNumeric>Hola</Td>
-                               <Td isNumeric>ETH</Td>
-                             </Tr>
-                             <Tr>
-                               <Td isNumeric>1</Td>
-                               <Td isNumeric>0X123</Td>
-                               <Td isNumeric>0X234</Td>
-                               <Td isNumeric>0X567</Td>
-                               <Td isNumeric>Hola</Td>
-                               <Td isNumeric>ETH</Td>
-                             </Tr>
-                             
-                           </Tbody>
-                            <TableCaption>Super Chat Analytics</TableCaption>
-                         </Table> 
-                        }
+                            <SuperChatGraph />
                         </Route>
                         <Route exact  path={`${path}/fundraisers`}>
-                            {
-                                <Heading>Fundraisers</Heading>
-                            }
+                            <FundGraph />
                         </Route>
                     </Switch>
                 </VStack>
